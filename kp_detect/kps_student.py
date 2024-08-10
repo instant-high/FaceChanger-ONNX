ffc94ffbc9aa494bd478c90c7bc0462cf4f329e6
Thomas De <heinzelmann1963@gmail.com>
2024-08-10 16:49:25 +0200

    Add files via upload

---
+++ kp_detect/kps_student.py
@@ -0,0 +1,29 @@
+﻿import cv2
+import onnxruntime
+import onnx
+import numpy as np
+
+class KP:
+    def __init__(self, model_path="kps_student.onnx", device='cpu'):
+        session_options = onnxruntime.SessionOptions()
+        session_options.graph_optimization_level = onnxruntime.GraphOptimizationLevel.ORT_ENABLE_ALL
+        providers = ["CPUExecutionProvider"]
+        if device == 'cuda':
+            providers = [("CUDAExecutionProvider", {"cudnn_conv_algo_search": "DEFAULT"}),"CPUExecutionProvider"]
+        self.session = onnxruntime.InferenceSession(model_path, sess_options=session_options, providers=providers)
+        model = onnx.load(model_path)
+
+        
+    def get_kp(self, image):
+    
+        image = cv2.resize(image, (256, 256))
+        image = image.astype(np.float32)
+        image = image.transpose((2, 0, 1)) / 255.0
+        image = np.expand_dims(image, axis=0).astype(np.float32)
+                
+        kp = self.session.run(None,{'input':image})[0]
+        
+        kp = np.array(kp)[:98*2].reshape(-1,2)
+        kp = kp * [256,256]
+
+        return kp
